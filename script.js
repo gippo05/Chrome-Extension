be80303fab0e52cffdb8668f62bf6e899f732b00
gippo05 <gsarinas17@gmail.com>
2025-03-31 12:12:30 +0800

    modified js file

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ulEl = document.getElementById('ul-el');
 
 saveButton.addEventListener("click", function(){
     myLeads.push(inputEl.value);
+    inputEl.value = "";
     renderLeads();
 });
 
@@ -12,15 +13,7 @@ saveButton.addEventListener("click", function(){
 function renderLeads(){
 let lisItems = [];
 for (let i = 0; i < myLeads.length; i++){
-    lisItems += "<li>" + myLeads[i]
+    lisItems += "<li><a target = '_blank' href='"+ myLeads[i] + "'>" + myLeads[i] + "</a></li>"
 }
 ulEl.innerHTML = lisItems;
-}
-
-let renderTwice = twiceLeads();
-
-function twiceLeads(){
-    for (let i = 0; i < 3; i++){
-        
-    }
 }
