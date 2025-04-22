d8f41157b453d9efb7d10cade228a85afe576f6e
gippo05 <gsarinas17@gmail.com>
2025-04-22 13:37:40 +0800

    MODIFIED JS

--- script.js
+++ script.js
@@ -2,25 +2,44 @@ let saveButton = document.getElementById('input-btn');
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const deleteButton = document.getElementById('delete-btn')
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads){
+    let lisItems = [];
+    for (let i = 0; i < leads.length; i++){
+        lisItems += `
+                <li>
+                    <a target='_blank' href='${leads[i]}'>
+                        ${leads[i]}
+                    </a>
+                </li> 
+            `
+            
+    }
+    ulEl.innerHTML = lisItems;
+    }
+    
 
 saveButton.addEventListener("click", function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    renderLeads();
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
 });
 
+deleteButton.addEventListener("dblclick", () => {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
 
-function renderLeads(){
-let lisItems = [];
-for (let i = 0; i < myLeads.length; i++){
-    lisItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li> 
-        `
-        
-}
-ulEl.innerHTML = lisItems;
-}
+
+
